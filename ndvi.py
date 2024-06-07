7fd3da2eae08f05d801b43e4304768455ef65878
Júlia Rodrigues <105869092+juliarmn@users.noreply.github.com>
2024-06-07 12:12:29 -0300

    Update ndvi.py

--- ndvi.py
+++ ndvi.py
@@ -11,13 +11,11 @@ imagem_corte = imagem.clipToBoundsAndScale(geometry=geometria,scale=40)
 imagem_corte = ndvi(imagem_corte)
 print(imagem.bandNames().getInfo())
 
-#Pegar imagem:
 print(imagem_corte.select(['ndvi']).getDownloadUrl())
 
 print()
 print(imagem_corte.select(['ndvi']).getThumbUrl({'min':-1, 'max':1}))
 
-#rgb:
 print(imagem_corte.select(['B4','B3','B2']).getDownloadUrl())
 
 print()
