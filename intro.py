eb9f76e9b8425fad4c12b78c2fd2f43ebbcba28b
Júlia Rodrigues <105869092+juliarmn@users.noreply.github.com>
2024-06-07 12:14:28 -0300

    Update intro.py

--- intro.py
+++ intro.py
@@ -12,17 +12,16 @@ geometria = geometry = ee.Geometry.Polygon (
                                                 ]
                                             ]
                                             )
-#Queremos as coordenadas centrais da área:
+
 longitude_centro = (float(y2) + float(y1)) / 2
 latitude_centro = (float(x2) + float(x1)) / 2
-#Definir por data as imagens que queremos:
+
 data = "2017-01-01,2017-12-31"
-#Unpacking:
+
 inicio, fim = data.split(",")
 print(inicio)
 print(fim)
-#Filtrar coleções:
-#1 - Pegar a coleção desejada, vamos usar o landsat-08
+
 colecao = ee.ImageCollection("LANDSAT/LC08/C02/T1").filterBounds(geometria).filterDate(inicio, fim).filterMetadata('CLOUD_COVER', 'less_than', 100)
-#Ver o total de imagens disponíveis:
+
 print(colecao.size().getInfo())
